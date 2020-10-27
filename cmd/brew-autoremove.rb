6471d3ce55a922e9abd7380a1d8ef4376da1650a
exuanbo <exuanbo@protonmail.com>
2020-10-27 15:23:06 +0000

    refactor: extract function, rename variable

--- cmd/brew-autoremove.rb
+++ cmd/brew-autoremove.rb
@@ -5,22 +5,27 @@ module BrewAutoremove
     `brew list --formula`.split
   end
 
-  def main
-    pkgs_to_remove = ARGV.map { |pkg| [pkg, *`brew deps #{pkg}`.split] }
+  def remove_pkg_and_deps(pkg)
+    pkg_count = list_formula.count
 
-    pkgs_to_remove.each do |_pkgs|
+    loop do
+      still_installed = pkg & list_formula
+      still_installed.each { |pkg| system("brew uninstall #{pkg} 2>/dev/null") }
+
+      pkg_count_old = pkg_count
       pkg_count = list_formula.count
 
-      loop do
-        still_installed = _pkgs & list_formula
-        still_installed.each { |pkg| system "brew uninstall #{pkg} 2>/dev/null" }
-
-        pkg_count_old = pkg_count
-        pkg_count = list_formula.count
-
-        break if pkg_count == pkg_count_old
-      end
+      break if pkg_count == pkg_count_old
     end
+  end
+
+  def main
+    pkgs_to_remove = ARGV.map { |pkg| [pkg, *`brew deps #{pkg}`.split] }
+    pkgs_to_remove.each do |pkg|
+      remove_pkg_and_deps(pkg)
+    end
+
+    system('brew cleanup')
   end
 end
 
